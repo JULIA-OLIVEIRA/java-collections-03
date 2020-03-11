2c309dececabdb50299afd605196227c374df242
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 20:55:43 -0300

    alteracao

---
+++ src/br/fai/collections03/Animal.java
@@ -0,0 +1,44 @@
+package br.fai.collections03;
+
+public abstract class Animal {
+
+	public static void som() {
+		
+		System.out.println("este é o som padrão de um animal: zzzz");
+
+	}
+	
+	public void dizerMeuNome() {
+		System.out.println("o nome do animal é:  xxxxx  ");
+	}
+	
+	private void dizerMinhaIdade() {
+		
+		System.out.println("minha idade é: yyyy ");
+	}
+	
+	private String cor = "preto";
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	private String cor = "preto";
+	private int idade = 5;
+	private String nome = "não definido";
+}
---
+++ src/br/fai/collections03/Cachorro.java
@@ -0,0 +1,10 @@
+package br.fai.collections03;
+
+public class Cachorro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
